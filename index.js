dc69f95584dcf20f46ed31ec37cff7acf313cca5
jeremybouldin <56416889+jeremybouldin@users.noreply.github.com>
2023-01-20 15:35:59 -0500

    converted the index.js file to a function

--- index.js
+++ index.js
@@ -1,22 +1,43 @@
-const heroEl = document.getElementById('hero')
-const monsterEl = document.getElementById('monster')
+const hero = 'hero'
+const heroName = 'Wizard'
+const heroAvatar = 'images/wizard.png'
+const heroHealth = 60
+const heroDiceContainer = 6
 
-// Render hero
+const monster = 'monster'
+const monsterName = 'Orc'
+const monsterAvatar = "images/orc.png"
+const monsterHealth = 10
+const monsterDiceContainer = 4
 
-   heroEl.innerHTML = `
+function renderCharacter(character, name, avatar, health, dice) {
+   document.getElementById(character).innerHTML = `
    <div class="character-card">
-      <h4 class="name"> Wizard </h4>
-      <img class="avatar" src="images/wizard.png"/>
-      <p class="health">health: <b> 60 </b></p>
-      <div class="dice-container"><div class="dice"> 6 </div></div>
+      <h4 class="name"> ${name} </h4>
+      <img class="avatar" src="${avatar}"/>
+      <p class="health">health: <b> ${health} </b></p>
+      <div class="dice-container"><div class="dice"> ${dice} </div>
    </div>
-`
+   `
+}
 
-monsterEl.innerHTML = `
-   <div class="character-card">
-      <h4 class="name">Orc</h4>
-      <img class="avatar" src="images/orc.png" />
-      <p class="health">health: <b> 10 </b></p>
-      <div class="dice-container"><div class="dice">4</div></div>
-   </div>
-`
+
+renderCharacter(hero, heroName, heroAvatar, heroHealth, heroDiceContainer)
+renderCharacter(monster, monsterName, monsterAvatar, monsterHealth, monsterDiceContainer)
+// document.getElementById('hero').innerHTML = `
+// <div class="character-card">
+// <h4 class="name"> Wizard </h4>
+// <img class="avatar" src="images/wizard.png"/>
+// <p class="health">health: <b> 60 </b></p>
+// <div class="dice-container"><div class="dice"> 6 </div></div>
+// </div>
+// `
+
+// document.getElementById('monster').innerHTML = `
+//    <div class="character-card">
+//       <h4 class="name">Orc</h4>
+//       <img class="avatar" src="images/orc.png" />
+//       <p class="health">health: <b> 10 </b></p>
+//       <div class="dice-container"><div class="dice">4</div></div>
+//    </div>
+// `
