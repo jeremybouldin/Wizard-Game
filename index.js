3d4ece3506f77970429b9ad2f2abd1ec8deedb40
jeremybouldin <56416889+jeremybouldin@users.noreply.github.com>
2023-01-23 18:07:49 -0500

    Destructured the hero and monster objects

--- index.js
+++ index.js
@@ -1,43 +1,31 @@
-const hero = 'hero'
-const heroName = 'Wizard'
-const heroAvatar = 'images/wizard.png'
-const heroHealth = 60
-const heroDiceContainer = 6
+const hero = {
+   elementId: 'hero', 
+   name: 'Wizard',
+   avatar: 'images/wizard.png',
+   health: 60,
+   diceContainer: 6
+}
 
-const monster = 'monster'
-const monsterName = 'Orc'
-const monsterAvatar = "images/orc.png"
-const monsterHealth = 10
-const monsterDiceContainer = 4
+const monster ={
+   elementId: 'monster', 
+   name: 'Orc',
+   avatar: "images/orc.png",
+   health: 10,
+   diceContainer: 4
+}
 
-function renderCharacter(character, name, avatar, health, dice) {
-   document.getElementById(character).innerHTML = `
+
+function renderCharacter(data) {
+   const {elementId, name, avatar, health, diceContainer} = data
+   document.getElementById(elementId).innerHTML = `
    <div class="character-card">
       <h4 class="name"> ${name} </h4>
       <img class="avatar" src="${avatar}"/>
       <p class="health">health: <b> ${health} </b></p>
-      <div class="dice-container"><div class="dice"> ${dice} </div>
+      <div class="dice-container"><div class="dice"> ${diceContainer} </div>
    </div>
    `
 }
 
-
-renderCharacter(hero, heroName, heroAvatar, heroHealth, heroDiceContainer)
-renderCharacter(monster, monsterName, monsterAvatar, monsterHealth, monsterDiceContainer)
-// document.getElementById('hero').innerHTML = `
-// <div class="character-card">
-// <h4 class="name"> Wizard </h4>
-// <img class="avatar" src="images/wizard.png"/>
-// <p class="health">health: <b> 60 </b></p>
-// <div class="dice-container"><div class="dice"> 6 </div></div>
-// </div>
-// `
-
-// document.getElementById('monster').innerHTML = `
-//    <div class="character-card">
-//       <h4 class="name">Orc</h4>
-//       <img class="avatar" src="images/orc.png" />
-//       <p class="health">health: <b> 10 </b></p>
-//       <div class="dice-container"><div class="dice">4</div></div>
-//    </div>
-// `
+renderCharacter(hero)
+renderCharacter(monster)
