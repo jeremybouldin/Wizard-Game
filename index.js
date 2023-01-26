71ea63c3d1f500234b3f2528ffbc8e10df413053
jeremybouldin <56416889+jeremybouldin@users.noreply.github.com>
2023-01-26 16:34:00 -0500

    Tidied up JS with constructor function
    Used object.assign to simplify the creation of the constructor function.
    Moved some formulas inside the constructor function.
    Added object destructuring to assign the properties to 'this.'

--- index.js
+++ index.js
@@ -14,32 +14,41 @@ const monster ={
    diceCount: 1,
 }
 
+function Character(data){
+   Object.assign(this, data);
+   this.getCharacterHtml = function(){
+      const {elementId, name, avatar, health, diceCount} = this
+      let diceHtml = this.getDiceHtml(diceCount)
+
+      document.getElementById(elementId).innerHTML = `
+         <div class="character-card">
+            <h4 class="name"> ${name} </h4>
+            <img class="avatar" src="${avatar}"/>
+            <p class="health">health: <b> ${health} </b></p>
+            <div class="dice-container">
+               ${diceHtml}
+            </div>
+         </div>
+      `
+   }
+   
+   this.getDiceHtml = function(diceCount){
+      return getDiceRollArray(diceCount).map(function(num){
+         return `<div class="dice">${num}</div>`
+      }).join('')
+   }
+
+}
+
+const wizard = new Character(hero)
+const orc = new Character(monster)
+
+wizard.getCharacterHtml()
+orc.getCharacterHtml()
+
 function getDiceRollArray(diceCount){
    return new Array(diceCount).fill(0).map(function(){
       return Math.floor(Math.random()*(6)+1)
    })
 }
 
-function getDiceHtml(diceCount){
-   return getDiceRollArray(diceCount).map(function(num){
-      return `<div class="dice">${num}</div>`
-   }).join('')
-}
-
-function renderCharacter(data) {
-   const {elementId, name, avatar, health, diceCount} = data
-
-   document.getElementById(elementId).innerHTML = `
-   <div class="character-card">
-      <h4 class="name"> ${name} </h4>
-      <img class="avatar" src="${avatar}"/>
-      <p class="health">health: <b> ${health} </b></p>
-      <div class="dice-container">
-         ${getDiceHtml(diceCount)}
-      </div>
-   </div>
-   `
-} 
-
-renderCharacter(hero)
-renderCharacter(monster)
