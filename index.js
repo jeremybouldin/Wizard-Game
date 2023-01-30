86fb96e90d01c2ee17bb822d224ab18cc0bfc455
jeremybouldin <56416889+jeremybouldin@users.noreply.github.com>
2023-01-30 10:14:00 -0500

    Moved constructor function to its own file

--- index.js
+++ index.js
@@ -1,37 +1,11 @@
-import {characterData} from "./data.js";
-import { getDiceRollArray } from "./utils.js";
+import { characterData } from "./data.js";
+import Character from "./Character.js";
 
-function Character(data){
-   Object.assign(this, data);
-   this.getCharacterHtml = function(){
-      const {elementId, name, avatar, health, diceCount} = this
-      let diceHtml = this.getDiceHtml(diceCount)
-
-      return `
-         <div class="character-card">
-            <h4 class="name"> ${name} </h4>
-            <img class="avatar" src="${avatar}"/>
-            <p class="health">health: <b> ${health} </b></p>
-            <div class="dice-container">
-               ${diceHtml}
-            </div>
-         </div>
-      `
-   }
-   
-   this.getDiceHtml = function(diceCount){
-      return getDiceRollArray(diceCount).map(function(num){
-         return `<div class="dice">${num}</div>`
-      }).join('')
-   }
-
+function render(){
+   document.getElementById('hero').innerHTML = wizard.getCharacterHtml()
+   document.getElementById('monster').innerHTML = orc.getCharacterHtml()
 }
 
 const wizard = new Character(characterData.hero)
 const orc = new Character(characterData.monster)
-function render(){
-   document.getElementById(wizard.elementId).innerHTML = wizard.getCharacterHtml()
-   document.getElementById(orc.elementId).innerHTML = orc.getCharacterHtml()
-}
-
 render()
