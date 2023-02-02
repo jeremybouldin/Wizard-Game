17d3ce9fa02221e240a3d437ca446a3a6fd4d49c
jeremybouldin <56416889+jeremybouldin@users.noreply.github.com>
2023-02-02 14:14:54 -0500

    Converted constructor to Class

--- Character.js
+++ Character.js
@@ -3,26 +3,93 @@ import {
     getDicePlaceholderHtml,
     getPercentage,
 } from './utils.js'
-export default Character
 
-function Character(data) {
-    Object.assign(this, data)
-    this.diceHtml = getDicePlaceholderHtml(this.diceCount)
-    this.maxHealth = this.health
+// function Character(data) {
+//     Object.assign(this, data)
+//     this.diceHtml = getDicePlaceholderHtml(this.diceCount)
+//     this.maxHealth = this.health
 
-    this.getHealthBarHtml = function () {
+//     this.getHealthBarHtml = function () {
+//         const percent = getPercentage(this.health, this.maxHealth)
+//         return `
+//          <div class="health-bar-outer">
+//             <div class="health-bar-inner ${
+//                 percent <= 25 ? 'danger' : ''
+//             }" style="width: ${percent}%">
+//             </div>
+//          </div>
+//       `
+//     }
+
+//     this.setDiceHtml = function (diceCount) {
+//         this.currentDiceScore = getDiceRollArray(this.diceCount)
+
+//         //Arrow function
+//         this.diceHtml = this.currentDiceScore
+//             .map((num) => `<div class="dice">${num}</div>`)
+//             .join('')
+
+//         //Inline function
+//         // this.diceArray = this.currentDiceScore.map(function(num){
+//         //    return `<div class="dice">${num}</div>`
+//         // }).join('')
+//     }
+
+//     this.takeDamage = function (attackScoreArray) {
+//         //Arrow function
+//         const totalAttackScore = attackScoreArray.reduce(
+//             (total, currentScore) => total + currentScore
+//         )
+
+//         //Inline function
+//         // const totalAttackScore = attackScoreArray.reduce(function(total, currentScore){
+//         //    return total + currentScore
+//         // })
+
+//         this.health -= totalAttackScore
+//         if (this.health <= 0) {
+//             this.dead = true
+//             this.health = 0
+//         }
+//     }
+
+//     this.getCharacterHtml = function () {
+//         const { name, avatar, health, diceHtml } = this
+//         const healthBar = this.getHealthBarHtml()
+
+//         return `
+//          <div class="character-card">
+//             <h4 class="name"> ${name} </h4>
+//             <img class="avatar" src="${avatar}"/>
+//             <p class="health">health: <b> ${health} </b></p>
+//             ${healthBar}
+//             <div class="dice-container">
+//                ${diceHtml}
+//             </div>
+//          </div>
+//       `
+//     }
+// }
+
+class Character {
+    constructor(data) {
+        Object.assign(this, data)
+        this.diceHtml = getDicePlaceholderHtml(this.diceCount)
+        this.maxHealth = this.health
+    }
+    getHealthBarHtml() {
         const percent = getPercentage(this.health, this.maxHealth)
         return `
-         <div class="health-bar-outer">
+            <div class="health-bar-outer">
             <div class="health-bar-inner ${
                 percent <= 25 ? 'danger' : ''
             }" style="width: ${percent}%">
             </div>
-         </div>
-      `
+            </div>
+        `
     }
 
-    this.setDiceHtml = function (diceCount) {
+    setDiceHtml() {
         this.currentDiceScore = getDiceRollArray(this.diceCount)
 
         //Arrow function
@@ -36,7 +103,7 @@ function Character(data) {
         // }).join('')
     }
 
-    this.takeDamage = function (attackScoreArray) {
+    takeDamage(attackScoreArray) {
         //Arrow function
         const totalAttackScore = attackScoreArray.reduce(
             (total, currentScore) => total + currentScore
@@ -54,20 +121,22 @@ function Character(data) {
         }
     }
 
-    this.getCharacterHtml = function () {
+    getCharacterHtml() {
         const { name, avatar, health, diceHtml } = this
         const healthBar = this.getHealthBarHtml()
 
         return `
-         <div class="character-card">
+            <div class="character-card">
             <h4 class="name"> ${name} </h4>
             <img class="avatar" src="${avatar}"/>
             <p class="health">health: <b> ${health} </b></p>
             ${healthBar}
             <div class="dice-container">
-               ${diceHtml}
+                ${diceHtml}
             </div>
-         </div>
-      `
+            </div>
+        `
     }
 }
+
+export default Character
